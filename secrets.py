22bcbbdf22daea8b3adaa19840631fba1505adaf
vinay9395 <163309318+vinay9395@users.noreply.github.com>
2024-06-10 21:34:22 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -7,9 +7,7 @@ if not os.environ.get("TOKEN") or not os.environ.get("DATABASE_URL"):
 from telegram.ext import Filters
 
 BOT_TOKEN = os.environ.get("7236081501:AAHyQmRW45_VKsl_Rglo42p63CmwIEMxfHw")
-DB_URI = os.environ.get("JsUNRj65LRfE9tFh
-godpresentfr
-mongodb+srv://godpresentfr:JsUNRj65LRfE9tFh@vanshfr.p4mhoke.mongodb.net/?retryWrites=true&w=majority&appName=Vanshfr")
+DB_URI = os.environ.get("mongodb+srv://godpresentfr:JsUNRj65LRfE9tFh@vanshfr.p4mhoke.mongodb.net/?retryWrites=true&w=majority&appName=Vanshfr")
 SUDO_USERS = [
     5086819565
 ]

317a0a2ebcb8a7935b4e643f50150e1b33369021
vinay9395 <163309318+vinay9395@users.noreply.github.com>
2024-06-10 21:42:52 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -4,7 +4,7 @@ if not os.environ.get("TOKEN") or not os.environ.get("DATABASE_URL"):
     print("Please specify TOKEN and DATABASE_URL environment variables before starting the bot.")
     exit()
 
-from telegram.ext import Filters
+from telegram.ext import filters
 
 BOT_TOKEN = os.environ.get("TOKEN")
 DB_URI = os.environ.get("DATABASE_URL")
